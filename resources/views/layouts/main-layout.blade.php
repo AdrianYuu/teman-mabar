79ea7dbb9f6aa2fc7579ed94f5b461c2c2ae0903
Adrian Yu <adrianyuu01@gmail.com>
2024-10-02 20:11:44 +0700

    initial commit

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TemanMabar</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="text-3xl font-bold text-white bg-green-500 min-h-screen flex justify-center items-center">
+        Teman Mabar
+    </div>
+</body>
+</html>
