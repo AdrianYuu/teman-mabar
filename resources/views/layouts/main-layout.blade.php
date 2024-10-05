2fbdc45f2814fbc881c6b2ff6f5004e47e7da7e2
BernardBerenes <bernardbereness78@gmail.com>
2024-10-05 09:20:36 +0700

    feat: add navbar & footer

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TemanMabar</title>
+    <title>@yield('title', '')</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="text-3xl font-bold text-white bg-green-500 min-h-screen flex justify-center items-center">
-        Teman Mabar
+    <div class="w-full min-h-screen bg-green-50">
+        @include('layouts.navbar')
+        @yield('content')
+        @include('layouts.footer')
     </div>
 </body>
 </html>
