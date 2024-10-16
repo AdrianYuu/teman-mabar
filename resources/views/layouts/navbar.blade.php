4e328638ec8215133e42a69b73a6f6bfaed6baee
BernardBerenes <bernardbereness78@gmail.com>
2024-10-16 19:39:22 +0700

    feat: add login & register button navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,34 +7,41 @@
 		</a>
 		{{-- Profile Dropdown --}}
 		<div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-			<button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-			<span class="sr-only">Open user menu</span>
-			<img class="w-8 h-8 rounded-full" src={{ asset('assets/images/profile-picture.jpg') }} alt="user photo">
-			</button>
-			<div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-				<div class="px-4 py-3">
-					<span class="block text-sm text-gray-900 dark:text-white">Bernard Bereness</span>
-					<span class="block text-sm text-gray-500 truncate dark:text-gray-400">bernardbereness@gmail.com</span>
+			@auth
+				<button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+				<span class="sr-only">Open user menu</span>
+				<img class="w-8 h-8 rounded-full" src={{ asset('assets/images/profile-picture.jpg') }} alt="user photo">
+				</button>
+				<div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+					<div class="px-4 py-3">
+						<span class="block text-sm text-gray-900 dark:text-white"></span>
+						<span class="block text-sm text-gray-500 truncate dark:text-gray-400"></span>
+					</div>
+					<ul class="py-2" aria-labelledby="user-menu-button">
+						<li>
+							<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile Saya</a>
+						</li>
+						<li>
+							<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Isi Saldo</a>
+						</li>
+						<form method="POST" action="{{ route('logout') }}">
+							@csrf
+							<button class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white w-full text-left">Keluar</button>
+						</form>
+					</ul>
 				</div>
-				<ul class="py-2" aria-labelledby="user-menu-button">
-					<li>
-						<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile Saya</a>
-					</li>
-					<li>
-						<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Isi Saldo</a>
-					</li>
-					<form method="POST" action="{{ route('logout') }}">
-						@csrf
-						<button class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white w-full text-left">Keluar</button>
-					</form>
-				</ul>
-			</div>
-			<button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
-				<span class="sr-only">Open main menu</span>
-				<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-				</svg>
-			</button>
+				<button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+					<span class="sr-only">Open main menu</span>
+					<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+					</svg>
+				</button>
+			@endauth
+			@guest
+				<a href="{{ route('loginPage') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mr-4">Login</a>
+				<a href="{{ route('registerPage') }}" class="text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-400 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Register</a>
+			@endguest
+			{{-- <p>{{ Auth::user()->name }}</p> --}}
 		</div>
 		<!-- Main Navbar -->
 		<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
