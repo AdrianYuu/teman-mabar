8afd840ee981cff9749d5593bfc8c898266c43b2
BernardBerenes <bernardbereness78@gmail.com>
2024-10-17 00:56:42 +0700

    feat: add tournament poster, coin icon

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -7,10 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', '')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
 </head>
 
 <body>
-    <div class="min-h-screen w-full bg-gray-50">
+    <div class="min-h-screen w-full bg-gray-500">
         @include('layouts.navbar')
         <div class="my-12 flex justify-center">
             @yield('content')
