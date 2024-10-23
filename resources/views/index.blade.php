1be7702dc54b0cf4ed5c907fb17920b17891b27b
BernardBerenes <bernardbereness78@gmail.com>
2024-10-24 01:33:31 +0700

    feat: update navbar, login regis form, add top player

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,7 @@
                 </div>
             </div>
             <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-1 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                     <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
                     </svg>
@@ -46,7 +46,7 @@
                 </span>
             </button>
             <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-1 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                     <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                     </svg>
@@ -59,12 +59,12 @@
             <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
                 <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab" data-tabs-toggle="#default-tab-content" role="tablist">
                     @foreach ($genres as $genre)
-                        <li class="me-2" role="presentation">
+                        <li class="me-auto" role="presentation">
                             <button class="inline-block p-4 border-b-2 rounded-t-lg" id="{{ strtolower($genre->name) }}-tab" data-tabs-target="#{{ strtolower($genre->name) }}" type="button" role="tab" aria-controls="{{ strtolower($genre->name) }}" aria-selected="false">{{ $genre->name }}</button>
                         </li>
                     @endforeach
-                    <li class="me-2">
-                        <a href="" class="inline-block p-4 rounded-t-lg text-blue-500">More...</a>
+                    <li>
+                        <a href="" class="inline-block p-4 rounded-t-lg text-blue-500 hover:border-b-2">More...</a>
                     </li>
                 </ul>
             </div>
@@ -79,6 +79,86 @@
                     </div>
                 @endforeach
             </div>
+        </div>
+        {{-- Top Player --}}
+        <div id="default-carousel" class="relative w-full" data-carousel="static">
+            <div class="relative overflow-hidden rounded-lg md:h-96">
+                <!-- Section 1 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <div class="grid grid-cols-2 gap-4">
+                        <a href="#" class="flex flex-col items-center bg-gray-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full h-3/4 p-2 gap-2">
+                            <img class="w-1/3 h-full rounded-md" src={{ asset('assets/images/profile-picture.jpg') }} alt="">
+                            <div class="flex flex-col justify-between leading-normal">
+                                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Test Test test</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Test</p>
+                            </div>
+                        </a>
+                        <a href="#" class="flex flex-col items-center bg-gray-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full h-3/4 p-2 gap-2">
+                            <img class="w-1/3 h-full rounded-md" src={{ asset('assets/images/profile-picture.jpg') }} alt="">
+                            <div class="flex flex-col justify-between leading-normal">
+                                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Test Test test</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Test</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <!-- Section 2 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <div class="grid grid-cols-2 gap-4">
+                        <a href="#" class="flex flex-col items-center bg-gray-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full h-3/4 p-2 gap-2">
+                            <img class="w-1/3 h-full rounded-md" src={{ asset('assets/images/profile-picture.jpg') }} alt="">
+                            <div class="flex flex-col justify-between leading-normal">
+                                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Test Test test</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Test</p>
+                            </div>
+                        </a>
+                        <a href="#" class="flex flex-col items-center bg-gray-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full h-3/4 p-2 gap-2">
+                            <img class="w-1/3 h-full rounded-md" src={{ asset('assets/images/profile-picture.jpg') }} alt="">
+                            <div class="flex flex-col justify-between leading-normal">
+                                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Test Test test</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Test</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <!-- Section 3 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <div class="grid grid-cols-2 gap-4">
+                        <a href="#" class="flex flex-col items-center bg-gray-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full h-3/4 p-2 gap-2">
+                            <img class="w-1/3 h-full rounded-md" src={{ asset('assets/images/profile-picture.jpg') }} alt="">
+                            <div class="flex flex-col justify-between leading-normal">
+                                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Test Test test</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Test</p>
+                            </div>
+                        </a>
+                        <a href="#" class="flex flex-col items-center bg-gray-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full h-3/4 p-2 gap-2">
+                            <img class="w-1/3 h-full rounded-md" src={{ asset('assets/images/profile-picture.jpg') }} alt="">
+                            <div class="flex flex-col justify-between leading-normal">
+                                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Test Test test</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Test</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Previous button -->
+            <button type="button" class="absolute bottom-[29%] -start-8 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none bg-black" data-carousel-prev>
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-400 dark:bg-gray-800/30 group-hover:bg-gray-500 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70">
+                    <svg class="w-4 h-4 text-gray-100 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                    </svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <!-- Next button -->
+            <button type="button" class="absolute bottom-[29%] -end-10 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-400 dark:bg-gray-800/30 group-hover:bg-gray-500 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70">
+                    <svg class="w-4 h-4 text-gray-100 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
         </div>
     </div>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,54 +15,58 @@
             <form method="POST" action="{{ route('register') }}" class="max-w-sm mx-auto w-full">
                 @csrf
                 <label for="name-icon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <div class="flex">
+                    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M5 4.33334C5 5.98734 6.346 7.33334 8 7.33334C9.654 7.33334 11 5.98734 11 4.33334C11 2.67934 9.654 1.33334 8 1.33334C6.346 1.33334 5 2.67934 5 4.33334ZM13.3333 14H14V13.3333C14 10.7607 11.906 8.66668 9.33333 8.66668H6.66667C4.09333 8.66668 2 10.7607 2 13.3333V14H13.3333Z"/>
-                        </svg>                                                                             
-                    </div>
-                    <input type="text" id="name-icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ketik nama lengkap kamu disini..." name="name">
+                        </svg>
+                    </span>
+                    <input type="text" id="name-icon" class="rounded-r-md bg-gray-50 border text-gray-900 focus:border-gray-900 focus:border-solid focus:ring-0 focus:outline-none block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Ketik nama kamu disini..." name="name">
                 </div>
                 @error('name')
                     <p class="text-red-500">{{ $message }}</p>
                 @enderror
-                <label for="email-icon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4">Email</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+
+                <label for="email-address-icon" class="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <div class="flex">
+                    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
                             <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
                             <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
                         </svg>
-                    </div>
-                    <input type="text" id="email-icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ketik email kamu disini..." name="email">
+                    </span>
+                    <input type="text" id="email-address-icon" class="rounded-r-md bg-gray-50 border text-gray-900 focus:border-gray-900 focus:border-solid focus:ring-0 focus:outline-none block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Ketik email kamu disini..." name="email">
                 </div>
                 @error('email')
                     <p class="text-red-500">{{ $message }}</p>
                 @enderror
-                <label for="password-icon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4">Kata Sandi</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+
+                <label for="password-icon" class="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
+                <div class="flex">
+                    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" viewBox="0 0 12 15" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M2 4.33334C2 3.27248 2.42143 2.25506 3.17157 1.50492C3.92172 0.754771 4.93913 0.333344 6 0.333344C7.06087 0.333344 8.07828 0.754771 8.82843 1.50492C9.57857 2.25506 10 3.27248 10 4.33334H10.6667C11.0203 4.33334 11.3594 4.47382 11.6095 4.72387C11.8595 4.97392 12 5.31305 12 5.66668V12.3333C12 12.687 11.8595 13.0261 11.6095 13.2762C11.3594 13.5262 11.0203 13.6667 10.6667 13.6667H1.33333C0.979711 13.6667 0.640573 13.5262 0.390524 13.2762C0.140476 13.0261 0 12.687 0 12.3333V5.66668C0 5.31305 0.140476 4.97392 0.390524 4.72387C0.640573 4.47382 0.979711 4.33334 1.33333 4.33334H2ZM6 1.66668C6.70724 1.66668 7.38552 1.94763 7.88562 2.44773C8.38572 2.94782 8.66667 3.6261 8.66667 4.33334H3.33333C3.33333 3.6261 3.61428 2.94782 4.11438 2.44773C4.61448 1.94763 5.29276 1.66668 6 1.66668ZM7.33333 8.33334C7.33333 8.56739 7.27172 8.79731 7.15469 8.99999C7.03767 9.20268 6.86935 9.37099 6.66667 9.48801V10.3333C6.66667 10.5102 6.59643 10.6797 6.4714 10.8047C6.34638 10.9298 6.17681 11 6 11C5.82319 11 5.65362 10.9298 5.5286 10.8047C5.40357 10.6797 5.33333 10.5102 5.33333 10.3333V9.48801C5.07916 9.34125 4.88052 9.11472 4.76821 8.84356C4.65589 8.57239 4.63619 8.27175 4.71216 7.98825C4.78812 7.70475 4.95551 7.45423 5.18836 7.27556C5.4212 7.09688 5.7065 7.00002 6 7.00001C6.35362 7.00001 6.69276 7.14049 6.94281 7.39053C7.19286 7.64058 7.33333 7.97972 7.33333 8.33334Z" />
-                        </svg>                                                   
-                    </div>
-                    <input type="password" id="password-icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ketik kata sandi kamu disini..." name="password">
+                        </svg>
+                    </span>
+                    <input type="password" id="password-icon" class="rounded-r-md bg-gray-50 border text-gray-900 focus:border-gray-900 focus:border-solid focus:ring-0 focus:outline-none block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Ketik kata sandi kamu disini..." name="password">
                 </div>
                 @error('password')
                     <p class="text-red-500">{{ $message }}</p>
                 @enderror
-                <label for="password-confirmation-icon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4">Konfirmasi Kata Sandi</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+
+                <label for="password-confirmation-icon" class="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Konfirmasi Kata Sandi</label>
+                <div class="flex">
+                    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" viewBox="0 0 12 15" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M2 4.33334C2 3.27248 2.42143 2.25506 3.17157 1.50492C3.92172 0.754771 4.93913 0.333344 6 0.333344C7.06087 0.333344 8.07828 0.754771 8.82843 1.50492C9.57857 2.25506 10 3.27248 10 4.33334H10.6667C11.0203 4.33334 11.3594 4.47382 11.6095 4.72387C11.8595 4.97392 12 5.31305 12 5.66668V12.3333C12 12.687 11.8595 13.0261 11.6095 13.2762C11.3594 13.5262 11.0203 13.6667 10.6667 13.6667H1.33333C0.979711 13.6667 0.640573 13.5262 0.390524 13.2762C0.140476 13.0261 0 12.687 0 12.3333V5.66668C0 5.31305 0.140476 4.97392 0.390524 4.72387C0.640573 4.47382 0.979711 4.33334 1.33333 4.33334H2ZM6 1.66668C6.70724 1.66668 7.38552 1.94763 7.88562 2.44773C8.38572 2.94782 8.66667 3.6261 8.66667 4.33334H3.33333C3.33333 3.6261 3.61428 2.94782 4.11438 2.44773C4.61448 1.94763 5.29276 1.66668 6 1.66668ZM7.33333 8.33334C7.33333 8.56739 7.27172 8.79731 7.15469 8.99999C7.03767 9.20268 6.86935 9.37099 6.66667 9.48801V10.3333C6.66667 10.5102 6.59643 10.6797 6.4714 10.8047C6.34638 10.9298 6.17681 11 6 11C5.82319 11 5.65362 10.9298 5.5286 10.8047C5.40357 10.6797 5.33333 10.5102 5.33333 10.3333V9.48801C5.07916 9.34125 4.88052 9.11472 4.76821 8.84356C4.65589 8.57239 4.63619 8.27175 4.71216 7.98825C4.78812 7.70475 4.95551 7.45423 5.18836 7.27556C5.4212 7.09688 5.7065 7.00002 6 7.00001C6.35362 7.00001 6.69276 7.14049 6.94281 7.39053C7.19286 7.64058 7.33333 7.97972 7.33333 8.33334Z" />
-                        </svg>                                                   
-                    </div>
-                    <input type="password" id="password-confirmation-icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ketik konfirmasi kata sandi kamu disini..." name="password_confirmation">
+                        </svg>
+                    </span>
+                    <input type="password" id="password-confirmation-icon" class="rounded-r-md bg-gray-50 border text-gray-900 focus:border-gray-900 focus:border-solid focus:ring-0 focus:outline-none block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Ketik konfirmasi kata sandi kamu disini..." name="password_confirmation">
                 </div>
                 @error('password_confirmation')
                     <p class="text-red-500">{{ $message }}</p>
                 @enderror
+
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4">Daftar</button>
             </form>
             <div class="flex items-center my-4">
